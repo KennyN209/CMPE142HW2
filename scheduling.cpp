fe0cc90ad3dc300cf5f1e8b514e9f3750b5cbde7
SenatorTedCruz <35795986+SenatorTedCruz@users.noreply.github.com>
2019-04-30 23:23:53 -0700

    Update scheduling.cpp

--- scheduling.cpp
+++ scheduling.cpp
@@ -19,90 +19,17 @@ struct process
         cout << "X\n";
     }
 };
-//a function to get the total work time of the CPU
-int tot_time(vector<process> jobs)
-{
-    int sum = 0;
-    for (int i = 0; i < jobs.size(); i++)
-    {
-        sum += jobs[i].burst;
-    }
-    return sum;
-}
-//printer
-void printHeaders(vector<process> jobs)
-{
-    for (int i = 0; i < jobs.size(); i++)
-        cout << jobs[i].name << "\t";
-    cout << endl;
-}
-//it goeos second by second and prints checks which process should go next and print it
-void FCFS(vector<process> jobs)
-{
-    printHeaders(jobs);
-    sort(jobs.begin(), jobs.end(), [](process a, process b)
-         {
-             return a.arrival < b.arrival;
-             
-         });
-    int tot = tot_time(jobs);
-    for (int i = 0; i < jobs.size(); i++)
-    {
-        for (int j = 0; j < jobs[i].burst; j++)
-            jobs[i].print();
-    }
-}
-
-void RR(vector<process> jobs)
-{
-    //we first print the header
-    printHeaders(jobs);
-    //then we sort them upon arrival time
-    sort(jobs.begin(), jobs.end(), [](process a, process b) {return a.arrival < b.arrival; });
-    //we need the total time as we will simulate it as the process time
-    int tot = tot_time(jobs);
-    int cur = -1;
-    //a vector that will hold what to print
-    vector<process> toprint;
-    
-    //now we simulate the running time
-    for (int i = 0; i < tot; i++)
-    {
-        //first thing we need to update the here bool
-        for (size_t j = 0; j < jobs.size(); j++)
-        {
-            //if the arrival time is not yet equal to the current time, i, we dont change the here
-            if (jobs[j].arrival <= i)
-                jobs[j].here = 1;
-            //if it has done the burst, we remove it also be setting here to 0
-            if (jobs[j].burst < 1)
-                jobs[j].here = 0;
-        }
-        
-        int s = (cur + 1) % jobs.size();//mapping cur pointer to a position in vector
-        //as long as we are pointing to a job that is not here , move the pointer
-        while (!jobs[s].here)
-            s = (++cur + 1) % jobs.size();
-        //we add it to the CPU schedule, aka printing it
-        toprint.push_back(jobs[(cur + 1) % jobs.size()]);
-        //we update the burst time for that process
-        jobs[(cur++ + 1) % jobs.size()].burst--;
-    }
-    for (size_t i = 0; i < toprint.size(); i++)
-        toprint[i].print();//now we print the processes that are to be scheduled
-}
-
 
 int main()
 {
-    //read the processes and then make the vector of processes and call the function
+    
     ifstream file;
     string fileName;
     cout << "Enter file name: ";
     cin >> fileName;
     file.open(fileName, ios::in);
     
-    // error handling
+  
     if (file.fail())
     {
         cout << "Input file opening failed" << endl;
@@ -111,14 +38,7 @@ int main()
    
     
     
-    
-    //ifstream file("file.txt");
-    
-    //file.open("file.txt", ios::in);
-    
-    // error handling
-    
-    
+  
     vector<process> jobs;
     while(!file.eof())
     {
@@ -139,3 +59,77 @@ int main()
         RR(jobs);
     }
 }
+
+
+void printHeaders(vector<process> jobs)
+{
+    for (int i = 0; i < jobs.size(); i++)
+        cout << jobs[i].name << "\t";
+    cout << endl;
+}
+
+void FCFS(vector<process> jobs)
+{
+    printHeaders(jobs);
+    sort(jobs.begin(), jobs.end(), [](process a, process b)
+         {
+             return a.arrival < b.arrival;
+             
+         });
+    int tot = tot_time(jobs);
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        for (int j = 0; j < jobs[i].burst; j++)
+            jobs[i].print();
+    }
+}
+
+int tot_time(vector<process> jobs)
+{
+    int sum = 0;
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        sum += jobs[i].burst;
+    }
+    return sum;
+}
+
+void RR(vector<process> jobs)
+{
+   
+    printHeaders(jobs);
+   
+    sort(jobs.begin(), jobs.end(), [](process a, process b) {return a.arrival < b.arrival; });
+    
+    int tot = tot_time(jobs);
+    int cur = -1;
+   
+    vector<process> toprint;
+    
+    
+    for (int i = 0; i < tot; i++)
+    {
+        
+        for (size_t j = 0; j < jobs.size(); j++)
+        {
+            
+            if (jobs[j].arrival <= i)
+                jobs[j].here = 1;
+            
+            if (jobs[j].burst < 1)
+                jobs[j].here = 0;
+        }
+        
+        int s = (cur + 1) % jobs.size();
+        
+        while (!jobs[s].here)
+            s = (++cur + 1) % jobs.size();
+     
+        toprint.push_back(jobs[(cur + 1) % jobs.size()]);
+       
+        jobs[(cur++ + 1) % jobs.size()].burst--;
+    }
+    for (size_t i = 0; i < toprint.size(); i++)
+        toprint[i].print();
+
+
